5d91d087a8dc7595fd30cb472362f8c5fa7d3487
Dumindu-Bandara <dumindubandara1999@gmail.com>
2023-12-10 21:20:16 +0530

    Added comments

--- code/knn_classifier.py
+++ code/knn_classifier.py
@@ -8,22 +8,20 @@ import numpy as np
 
 
 # Configuration for the model
-
 with open("./configs/knn_clf_configs.yaml", "r") as file:
     config = yaml.safe_load(file)
 
 # Dataset
 dataset = tfds.data_source("oxford_iiit_pet", data_dir=config["data_dir"],download=False)
 
+# Device CPU/GPU
 device = "cuda" if torch.cuda.is_available() else "cpu"
 print(f"{device} is used for model training")
 
+# Load pretrained model and preprocess function
 model, preprocess = clip.load(config["clip_model_parameters"]["ViT_model"], device=device, download_root=config["clip_model_dir"])
 
-# print(model.named_parameters)
-# image = preprocess(Image.open("./clip_test_images/CLIP.png")).unsqueeze(0).to(device)
-# print(image.size())
-
+# Train and Test dataset holders
 train_size = len(dataset["train"])
 train_embeddings= np.zeros((train_size, config["clip_model_parameters"]["embedding_size"]))
 train_labels = np.zeros(train_size)
@@ -33,6 +31,7 @@ test_embeddings= np.zeros((test_size, config["clip_model_parameters"]["embedding
 test_labels = np.zeros(test_size)
 
 
+# Generate embeddings for Train and Test datasets. 
 with torch.no_grad():
     if config["save_train_test_embeddings"]:
         for i in range(len(dataset["train"])):
@@ -61,9 +60,11 @@ with torch.no_grad():
         with open(f"{config['save_embeddings_dir']}/test/test_labels.npy", "wb") as file:
             np.save(file, test_labels)
 
-print()
 
-# image = Image.fromarray(dataset["train"][0]["image"])
-# image = preprocess(Image.open("./clip_test_images/CLIP.png")).unsqueeze(0).to(device)
-# image_embedding = model.encode_image(image)
-# print(image_embedding.size())
+# Load Train and Test embeddings
+train_embeddings = np.load(f"{config['save_embeddings_dir']}/train/train_embeddings.npy")
+train_labels = np.load(f"{config['save_embeddings_dir']}/train/train_labels.npy")
+test_embeddings = np.load(f"{config['save_embeddings_dir']}/test/test_embeddings.npy")
+test_labels = np.load(f"{config['save_embeddings_dir']}/test/test_labels.npy")
+
+print()
